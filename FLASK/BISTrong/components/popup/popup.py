ebd741a0bda7f4ad3394110633091334237f3b95
Nitay Mayo <nitaymayo@gmail.com>
2023-02-18 15:27:33 +0100

    Delete FLASK directory

--- FLASK/BISTrong/components/popup/popup.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint, render_template
-
-# popup blueprint definition
-popup = Blueprint('popup', __name__, static_folder='static', static_url_path='/popup', template_folder='templates')
--- FLASK/LELOG/pages/product/static/js/Purchase Modal.js
+++
@@ -1,8 +0,0 @@
-function change() {
-    const div = document.getElementById(`schedule`)
-    if (div.style.height === `60px`){
-        div.style.height = `0px`;
-    } else{
-        div.style.height = `60px`;
-    }
-}
